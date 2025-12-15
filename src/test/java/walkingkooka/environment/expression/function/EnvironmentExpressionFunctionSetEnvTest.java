387f18abf353dcd7dbc811c24bb20ab9e8c9cb8e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-15 20:49:52 +1100

    EnvironmentExpressionFunctionSetEnvTest formatted

--- src/test/java/walkingkooka/environment/expression/function/EnvironmentExpressionFunctionSetEnvTest.java
+++ src/test/java/walkingkooka/environment/expression/function/EnvironmentExpressionFunctionSetEnvTest.java
@@ -73,7 +73,7 @@ public final class EnvironmentExpressionFunctionSetEnvTest extends EnvironmentEx
 
             @Override
             public <T> ExpressionEvaluationContext setEnvironmentValue(final EnvironmentValueName<T> name,
-                                                                                  final T value) {
+                                                                       final T value) {
                 checkEquals(VAR, name);
                 checkEquals(VALUE, value);
 
